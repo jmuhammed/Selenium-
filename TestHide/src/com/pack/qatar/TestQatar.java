65fb11e7ae14515e795526709b56fc1985836b98
jmuhammed <jmuhammed@prokarma.com>
2014-08-28 01:58:15 +0530

    Test Class alone

--- TestHide/src/com/pack/qatar/TestQatar.java
+++ TestHide/src/com/pack/qatar/TestQatar.java
@@ -1,4 +1,4 @@
-package com.pack.qatar;
+//package com.pack.qatar;
 import java.util.List;
 
 import org.openqa.selenium.*;
@@ -20,8 +20,8 @@ public class TestQatar {
 		//capability.setCapability(CapabilityType.ACCEPT_SSL_CERTS, true);
 		
 		
-	//	WebDriver driver = new ChromeDriver(options);
-		WebDriver driver=new FirefoxDriver();
+		WebDriver driver = new ChromeDriver(options);
+		//WebDriver driver=new FirefoxDriver();
 		JavascriptExecutor jex=(JavascriptExecutor)driver;
 		driver.get("http://www.qatarairways.com");
 		try {
